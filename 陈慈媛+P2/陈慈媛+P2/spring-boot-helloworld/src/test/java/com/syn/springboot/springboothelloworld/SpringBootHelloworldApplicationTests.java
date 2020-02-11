735d7ee3e1c2ac502c760c18cddd218fe3960ed1
cychen-china <59906361+cychen-china@users.noreply.github.com>
2020-02-11 16:16:00 +0800

    Add files via upload

---
+++ 陈慈媛+P2/陈慈媛+P2/spring-boot-helloworld/src/test/java/com/syn/springboot/springboothelloworld/SpringBootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syn.springboot.springboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
